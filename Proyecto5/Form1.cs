9ca1fe474e05e2602de8ab2e92268a6fcffd7d8d
Richardd334 <145384949+Richardd334@users.noreply.github.com>
2023-10-19 09:36:49 -0600

    version 3

--- Proyecto5/Form1.cs
+++ Proyecto5/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,10 +17,18 @@ namespace Proyecto5
         public Form1()
         {
             InitializeComponent();
+
+            //Agregar controladores de eventos TextChanged a los campos
+            txtEdad.TextChanged += ValidarEdad;
+            txtApellido.TextChanged += ValidarApellido;
+            txtEstatura.TextChanged += ValidarEstatura;
+            txtNombre.TextChanged += ValidarNombre;
+            txtTelefono.TextChanged += ValidarTelefono;
+
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
-        { 
+        {
             string nombres = txtNombre.Text;
             string apellidos = txtApellido.Text;
             string edad = txtEdad.Text;
@@ -39,22 +48,120 @@ namespace Proyecto5
                 genero = "Mujer";
             }
 
-            //Cadena con datos
+            //validar que los campos tengan el formato correcto
+            if (EsEnteroValido(edad) && EsDecimalValido(estatura) && EsEnteroValidoDe10Digitos(telefono) && EsTextoValido(nombres)
+                && EsTextoValido(apellidos))
+            {
+                //Cadena con datos
+                string datos = $"Nombre:{nombres}\r\nApellidos:{apellidos}\r\nTelefono:{telefono}\r\nEstatura: {estatura}\r\nEdad:{edad} años\r\nGenero:{genero}";
 
-            string datos = $"Nombre:{nombres}\r\nApellidos:{apellidos}\r\nTelefono:{telefono}\r\nEstatura: {estatura}\r\nEdad:{edad} años\r\nGenero:{genero}";
+                //guardar datos en un archivo de texto
+                string rutaArchivo = "C:/Users/dinog/OneDrive/Documentos/Textos PA/datos2.txt";
 
-            //guardar datos en un archivo de texto
+                bool archivoExiste = File.Exists(rutaArchivo);
 
-            string rutaArchivo = "C:/Users/dinog/OneDrive/Documentos/Textos PA/datos1.txt";
-            File.WriteAllText(rutaArchivo, datos);
+                if (archivoExiste == false)
+                {
+                    File.WriteAllText(rutaArchivo, datos);
+                }
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter(rutaArchivo, true))
+                    {
+                        if (archivoExiste)
+                        {
+                            writer.WriteLine();
+                        }
+                        writer.WriteLine(datos);
+                    }
+                }
+                //Mostrar mensajes
+                MessageBox.Show("Datos guardados con exito:\n\n" + datos, "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese datos válidos en los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
-            //Mostrar mensajes
 
-            MessageBox.Show("Datos guardados con exito:\n\n" + datos, "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            
-        
         }
 
+        private bool EsEnteroValido(string valor)
+        { 
+            int resultado;
+            return int.TryParse(valor, out resultado);
+        }
+        private bool EsDecimalValido(string valor)
+        {
+            decimal resultado;
+            return decimal.TryParse(valor, out resultado);
+        }
+        private bool EsEnteroValidoDe10Digitos(string valor)
+        {
+            long resultado;
+            return long.TryParse(valor, out resultado) && valor.Length == 10;
+        }
+
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"[a-zA-Z\s]+$");   //Solo letras y espacios
+        }
+
+        private void ValidarEdad(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsDecimalValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese una edad valida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarEstatura(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsDecimalValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese una estatura valida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarTelefono(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string input = textBox.Text;
+            if (input.Length>10)
+            {
+                if (!EsEnteroValidoDe10Digitos(input))
+                {
+                    MessageBox.Show("Por favor ingrese un numero de telefono valido de 10 digitos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.Clear();
+                }
+            }
+            else if (!EsEnteroValidoDe10Digitos(input))
+            {
+                MessageBox.Show("Por favor ingrese un numero de telefono valido de 10 digitos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ValidarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese un nombre valido (solo letras y espacios)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+        private void ValidarApellido(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese apellidos validos (solo letras y espacios)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             txtNombre.Clear();
@@ -66,4 +173,13 @@ namespace Proyecto5
             rbMujer.Checked = false;
         }
     }
+
 }
+
+
+
+
+
+
+
+
